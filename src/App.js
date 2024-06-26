7fb8140a5d9b24143900450139aa04e8a6fc8455
omar <baiomayomar@gmail.com>
2024-06-26 22:49:23 +0300

    fix fetching

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import MovieCard from './components/MovieCard';
 
 // API Key : 67d2139c
 
-const API_URL = 'http://www.omdbapi.com/?apikey=67d2139c'
+const API_URL = 'https://www.omdbapi.com/?apikey=67d2139c'
 
 const App = () => {
 
